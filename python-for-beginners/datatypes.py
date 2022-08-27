24c40070ebb68a272fa222d8f2663c8ccf4dc26e
Milan Bariya <milanbariya12@gmail.com>
2022-08-27 10:27:24 +0530

    Add: Datatype examples

---
+++ python-for-beginners/datatypes.py
@@ -0,0 +1,12 @@
+num = 4
+print("Type of num: ", type(num))
+
+name = "Milan"
+print("Type of name: ", type(name))
+
+price = 3.6
+print("Type of price: ", type(price))
+
+
+val = True
+print("Type of val: ", type(val))
